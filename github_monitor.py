d1c482caaea23b3eb19119a8a2207eaa0fedf4d8
Guilherme Sant'Ana <guilherme.sant_ana@outlook.com>
2018-11-13 00:08:05 -0200

    add token

--- github_monitor.py
+++ github_monitor.py
@@ -6,6 +6,7 @@ import time
 import sys
 import os
 import gi
+import getpass
 gi.require_version('Notify', '0.7')
 from gi.repository import Notify
 
@@ -19,8 +20,8 @@ def get_time(response):
     time = datetime.strptime(response['commit']['commit']['author']['date'],'%Y-%m-%dT%H:%M:%SZ')
     return time
 
-def make_request(url):
-    response = requests.get(url)
+def make_request(url,headers=None):
+    response = requests.get(url,headers)
     return response.json()
 
 def sendmessage(message):
@@ -42,13 +43,40 @@ def get_commit_info(request):
     message =  request['commit']['commit']['message']
     return message
 
+def store_token(token):
+    with open('token.txt','w') as f:
+        f.write(token)
+
+def acquire_token():
+    try:
+        lines = [line.rstrip('\n') for line in open('token.txt')]
+        return lines[0]
+    except FileNotFoundError:
+        return acquire_token_from_web()
+
+
+def acquire_token_from_web():
+    base_url = 'https://api.github.com/'
+    base_url += 'authorizations'
+    username = input('username: ')
+    password = getpass.getpass('password: ')
+    response = requests.post(base_url,auth=(username,password),data=json.dumps({'note':'github notifier'}))
+    print(response)
+    response = response.json()
+    print(response)
+    token = response['token']
+    return token
+
+token = acquire_token()
+headers = {'Authorization': 'token '+token}
 
 url = url+organization_or_user+"/"+repository+"/branches/"+branch
-response = make_request(url)
+response = make_request(url,headers)
 old_time = get_time(response)
 
 while(True):
-    request = make_request(url)
+    time.sleep(300)
+    request = make_request(url,headers)
     new_time = get_time(request)
     if new_time > old_time:
         message = get_commit_info(request)
@@ -56,4 +84,3 @@ while(True):
         old_time == new_time
     else:
         print("sleep " + str(datetime.now()))
-    time.sleep(300)
